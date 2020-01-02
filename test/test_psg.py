e7f8e79468099a1a9d7541e0d8e25f1985712ee1
Asgari, Ehsan <ehsaneddin.asgari@volkswagen.de>
2020-01-02 03:39:27 +0100

    comment tree

--- test/test_psg.py
+++ test/test_psg.py
@@ -6,25 +6,25 @@ from src.geno2pheno.pipeline import Geno2PhenoPipeline
 DATA_DIR = os.path.dirname(__file__) + "/../data"
 
 def test_ps_tob():
-    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas/test_pseudomona_TOB.yml", overwrite=False, cores=4,
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_TOB.yml", overwrite=False, cores=4,
                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
 
 
 def test_ps_mer():
-    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas/test_pseudomona_MER.yml", overwrite=False, cores=4,
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_MER.yml", overwrite=False, cores=4,
                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
 
 
 def test_ps_cip():
-    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas/test_pseudomona_CIP.yml", overwrite=False, cores=4,
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_CIP.yml", overwrite=False, cores=4,
                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
 
 def test_ps_col():
-    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas/test_pseudomona_COL.yml", overwrite=False, cores=4,
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_COL.yml", overwrite=False, cores=4,
                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
 
 def test_ps_cez():
-    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas/test_pseudomona_CEZ.yml", overwrite=False, cores=4,
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_CEZ.yml", overwrite=False, cores=4,
                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
 
 test_ps_mer()
