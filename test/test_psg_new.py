34bf551e4529e0064dd9e0acfb9bfe6d0dc5122a
Asgari, Ehsan <ehsaneddin.asgari@volkswagen.de>
2020-03-02 16:10:22 +0100

    modify for the final test

--- test/test_psg_new.py
+++ test/test_psg_new.py
@@ -5,28 +5,29 @@ sys.path.append(GENO2PHENO_DIR)
 from src.geno2pheno.pipeline import Geno2PhenoPipeline
 DATA_DIR = os.path.dirname(__file__) + "/../data"
 
+INP_DIR = '/net/sgi/metagenomics/nobackup/prot/projects/verify_genopheno/'
+
 def test_ps_tob():
-    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/classify_psg_dataset_tob.yml", overwrite=True, cores=4,
-                                    replace_list=[('%(PROJDIR)%', f"/Users/vwr33vv/Documents/techday/pseudomona/final/input_data/"),('%(config)%', f"{DATA_DIR}/configs/"),('%(DRUG)%','TOB'),('%(drug_full)%','Tobramycin'),('%(OUTDIR)%','/Users/vwr33vv/Documents/techday/pseudomona/final/input_data/results/')])
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/classify_psg_datasets_generic.yml", overwrite=True, cores=25,
+                                    replace_list=[('%(PROJDIR)%', f"{INP_DIR}"),('%(config)%', f"{DATA_DIR}/configs/"),('%(DRUG)%','TOB'),('%(drug_full)%','Tobramycin'),('%(OUTDIR)%',f"{INP_DIR}/results/")])
+
+def test_ps_mer():
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/classify_psg_datasets_generic.yml", overwrite=True, cores=25,
+                                    replace_list=[('%(PROJDIR)%', f"{INP_DIR}"),('%(config)%', f"{DATA_DIR}/configs/"),('%(DRUG)%','MER'),('%(drug_full)%','Meropenem'),('%(OUTDIR)%', f"{INP_DIR}/results/")])
 
 
+def test_ps_cip():
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/classify_psg_datasets_generic.yml", overwrite=True, cores=25,
+                                    replace_list=[('%(PROJDIR)%', f"{INP_DIR}"),('%(config)%', f"{DATA_DIR}/configs/"),('%(DRUG)%','CIP'),('%(drug_full)%','Ciprofloxacin'),('%(OUTDIR)%',f"{INP_DIR}/results/")])
 
-# def test_ps_mer():
-#     geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_MER.yml", overwrite=False, cores=4,
-#                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
-#
-#
-# def test_ps_cip():
-#     geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_CIP.yml", overwrite=False, cores=4,
-#                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
-#
-# def test_ps_col():
-#     geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_COL.yml", overwrite=False, cores=4,
-#                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
-#
-# def test_ps_cez():
-#     geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/pseudomonas_dir/test_pseudomona_CEZ.yml", overwrite=False, cores=4,
-#                                     replace_list=[('%(PROJDIR)%', f"{DATA_DIR}/test_data/pseudogenomics/"),('%(config)%', f"{DATA_DIR}/configs/")])
-#
+def test_ps_cez():
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/classify_psg_datasets_generic.yml", overwrite=True, cores=25,
+                                    replace_list=[('%(PROJDIR)%', f"{INP_DIR}"),('%(config)%', f"{DATA_DIR}/configs/"),('%(DRUG)%','CEZ'),('%(drug_full)%','Ceftazidim'),('%(OUTDIR)%',f"{INP_DIR}/results/")])
+
+def test_ps_col():
+    geno2pheno = Geno2PhenoPipeline(F"{DATA_DIR}/genyml_examples/classify_psg_datasets_generic.yml", overwrite=True, cores=25,
+                                    replace_list=[('%(PROJDIR)%', f"{INP_DIR}"),('%(config)%', f"{DATA_DIR}/configs/"),('%(DRUG)%','COL'),('%(drug_full)%','Colistin'),('%(OUTDIR)%',f"{INP_DIR}/results/")])
+
+
 test_ps_tob()
 
