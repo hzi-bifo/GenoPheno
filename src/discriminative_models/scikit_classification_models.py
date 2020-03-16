240dfc3347da1484a9d7abe8cc80bd5026a182ac
Asgari, Ehsan <ehsaneddin.asgari@volkswagen.de>
2020-03-16 12:41:40 +0100

    comment tree

--- src/discriminative_models/scikit_classification_models.py
+++ src/discriminative_models/scikit_classification_models.py
@@ -40,8 +40,9 @@ class SVM:
     def tune_and_eval(self, save_path, inner_cv_number_of_folds, outer_cv , optimizing_score='f1_macro', n_jobs=4, overwrite=True,
                   logger=None):
 
-        np.save('X',self.X)
+        np.save('X',self.X.toarray())
         np.save('Y.txt', self.Y)
+        ## TODO
         FileUtility.save_list('features.txt', self.feature_names)
         FileUtility.save_list('instances.txt', self.instances)
         tune_eval = TuneEvalSteps(self.X, self.Y, self.instances, outer_cv, inner_cv_number_of_folds)
